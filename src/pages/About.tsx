2328fddc9ddebb4f0b6d2e08065acc529bc38886
Ronit Roushan <ronit21102@gmail.com>
2024-05-15 23:19:15 +0530

    commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
 import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
-      <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
+      <img src="https://media.licdn.com/dms/image/D5603AQEKtub1jRPmVA/profile-displayphoto-shrink_800_800/0/1706811399506?e=1721260800&v=beta&t=O5fTWCLBHu3SOyuvRPU8i6wb1ehLxTtWqaqJt2UdNvE" alt="" className="profile-image" />
+      <h1 className="profile-text">Hi I am Ronit Roushan</h1>
       <h2 className="profile-text">The creator of this awesome pokedex</h2>
-      <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
-      </h4>
+      
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/Ronit21102">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
-          <FaYoutube />
-        </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+        <a href="https://www.linkedin.com/in/ronit-roushan-460a9b194/">
           <FaLinkedin />
         </a>
       </div>
